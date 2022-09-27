4d711bbee7ffad9e68319fafd2650607b0294c42
Yosida-Ayuto <ksw2170029@stu.o-hara.ac.jp>
2022-09-27 11:11:05 +0900

    base

--- config/settings.py
+++ config/settings.py
@@ -41,8 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 変更
-    'bulletim_board.app.Bulletim_boardConfig',
-    'accounts.app.AccountsConfig',
+    'bulletim_board.apps.Bulletim_boardConfig',
+    'accounts.apps.AccountsConfig',
     
     #変更1
     'django.contrib.sites',
