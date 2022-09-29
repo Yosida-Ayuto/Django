ec2b51bed32a203ad01eaa521eafed5f8a170192
Yosida-Ayuto <ksw2170029@stu.o-hara.ac.jp>
2022-09-29 10:23:22 +0900

    del.up

--- bulletim_board/urls.py
+++ bulletim_board/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('list/',views.Bulletim_boardListView.as_view(), name="list"),
     path('create/',views.Bulletim_boardCreateView.as_view(), name="create"),
     path('updata/<int:pk>',views.UpdateView.as_view(),name="update"),
+    path('delete/<int:pk>',views.DeleteView.as_view(),name="delete"),
+
 ]
